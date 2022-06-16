052bf68f82bf0c5416c691a46680707fadbaa50b
deanqwq233 <102376145+deanqwq233@users.noreply.github.com>
2022-06-16 14:19:29 +0800

    Add files via upload

--- Cmdgros.cpp
+++ Cmdgros.cpp
@@ -1,14 +1,21 @@
 ﻿#include<iostream>
+#include<windows.h>
 using namespace std;
-char intro[50][50]={""}
+char intro[50][50] = { "|               |               |","|       欢迎来到|Cmdgros，      |","|       现在开始|玩法练习       |","|               |               |","|遇到音符按回车 |       #####   |","|               |               |","|   #####       |               |","|               |               |","|               |               |","|最上面是判定线 | 音符从下往上走|","|左下角显示距离 | 右下角显示分数|","|               |               |","|               |               |"};
 int main()
 {
 	int i,j;
 	for (i = 0; i <= 24; i++)
 	{
+		cout << "|_______________________________|" << endl;
 		for (j = 0; j <= 6; j++)
 		{
-
+			printf("%s", intro[i+j]);
+			cout << endl;
 		}
+		cout << "|_______________________________|" << endl;
+		Sleep(500);
+		system("cls");
 	 }
+	return 0;
 }
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Cmdgros
-用命令行写出一个仿Phigros？
